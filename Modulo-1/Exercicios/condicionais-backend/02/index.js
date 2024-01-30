460ab892c178859115a0eda34cfe657044765974
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-01-30 17:56:21 -0300

    Resolução exercicio 2

--- Modulo-1/Exercicios/condicionais-backend/02/index.js
+++ Modulo-1/Exercicios/condicionais-backend/02/index.js
@@ -1,4 +1,9 @@
 const jogada1 = 5;
-const jogada2 = 3;
+const jogada2 = 2;
+const soma = jogada1 + jogada2
 
-//seu código aqui
+if(soma % 2 == 0) {
+  console.log("O jogador que escolheu PAR é o vencedor da rodada!")
+} else {
+  console.log("O jogador que escolheu IMPAR é o vencedor da rodada!")
+}
