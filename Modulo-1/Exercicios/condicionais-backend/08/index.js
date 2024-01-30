33031f2c5f46dfbb0771d76544ffe208c5ebd0f5
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-01-30 19:34:16 -0300

    Resolução exercício 8

--- Modulo-1/Exercicios/condicionais-backend/08/index.js
+++ Modulo-1/Exercicios/condicionais-backend/08/index.js
@@ -3,3 +3,10 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+if((idade < 12 || idade > 65) || (possuiPatologia === true) || (altura < 150)) {
+  console.log("ACESSO NEGADO")
+} else if((ehEstudante === true) || idade < 18) {
+  console.log("Preço: MEIA-ENTRADA 10 REAIS")
+} else {
+  console.log("Preço: 20 REAIS")
+}
