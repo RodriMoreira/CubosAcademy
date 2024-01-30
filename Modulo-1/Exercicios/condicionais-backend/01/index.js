7394ed1ab7b8d9f000ce967a932466e6327d89b1
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-01-30 17:51:30 -0300

    Resolução exercicio 1

--- Modulo-1/Exercicios/condicionais-backend/01/index.js
+++ Modulo-1/Exercicios/condicionais-backend/01/index.js
@@ -1,4 +1,15 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogador1 = "tesoura"
+const jogador2 = "tesoura"
 
-//seu código aqui
+if((jogador1 == "pedra" && jogador2 == "papel") ||
+   (jogador1 == "papel" && jogador2 == "tesoura") ||
+   (jogador1 == "tesoura" && jogador2 == "pedra")) {
+    //Jogador1 PERDEU
+    console.log("O jogador 2 ganhou!")
+} else if (jogador1 === jogador2) {
+    //Empate
+  console.log("Empate entre os jogadores!")
+} else {
+    //Jogador 1 GANHOU
+  console.log("O jogador 1 ganhou!")
+}
