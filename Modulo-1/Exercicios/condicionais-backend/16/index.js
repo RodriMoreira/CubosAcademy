abef3b923e682290f1f0b97cd4ec294c00c29416
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-01 20:22:08 -0300

    Resolução exercício 16

--- Modulo-1/Exercicios/condicionais-backend/16/index.js
+++ Modulo-1/Exercicios/condicionais-backend/16/index.js
@@ -3,3 +3,9 @@ const idadeDoAluno = 16;
 
 //valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = false;
+
+if((idadeDoAluno < 18 && possuiResponsavel === true) || idadeDoAluno > 18){
+  console.log("É possivel realzar a matricula")
+} else {
+  console.log("Não foi possivel realizar a matricula")
+}
