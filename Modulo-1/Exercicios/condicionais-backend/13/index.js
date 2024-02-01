473b2d9079c2c9fac75c6e14fba004c81bffec04
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-01 19:51:40 -0300

    Resolução exercício 13

--- Modulo-1/Exercicios/condicionais-backend/13/index.js
+++ Modulo-1/Exercicios/condicionais-backend/13/index.js
@@ -1,6 +1,18 @@
 //tipo de pagamento (dinheiro, credito, debito, cheque).
 const tipoDePagamento = "credito";
 
-//valor da mercadoria (centavos)
-const valorDoProduto = 13000;
+//valor da mercadoria (Reais)
+const valorDoProdutoReais = 130;
+const descontoCredito = (valorDoProdutoReais * (5 / 100)).toFixed(2)
+const descontoCheque = (valorDoProdutoReais * (3 / 100)).toFixed(2)
+const descontoDinheiroOuDebito = (valorDoProdutoReais * (10 / 100)).toFixed(2)
+if(tipoDePagamento === "credito") {
+  console.log(valorDoProdutoReais - descontoCredito)
+} else if(tipoDePagamento === "cheque") {
+  console.log(valorDoProdutoReais - descontoCheque)
+} else if(tipoDePagamento === "dinheiro" || tipoDePagamento === "debito") {
+  console.log(valorDoProdutoReais - descontoCheque)
+} else {
+  console.log("ERROR")
+}
 
