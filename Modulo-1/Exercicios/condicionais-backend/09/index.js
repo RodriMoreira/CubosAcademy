989361875acc43131df1e8e0c74cf0050160953e
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-01 19:15:25 -0300

    Resolução exercício 9

--- Modulo-1/Exercicios/condicionais-backend/09/index.js
+++ Modulo-1/Exercicios/condicionais-backend/09/index.js
@@ -1,1 +1,13 @@
 const nota = 8.5;
+
+if(nota < 4) {
+  console.log("Conceito E")
+} else if(nota < 5.9) {
+  console.log("Conceito D")
+} else if(nota < 7.9) {
+  console.log("Conceito C")
+} else if(nota < 8.9) {
+  console.log("Conceito B")
+} else {
+  console.log("Conceito A")
+}
