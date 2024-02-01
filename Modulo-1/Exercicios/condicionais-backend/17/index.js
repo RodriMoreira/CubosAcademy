f90fcb3997c768b1962d5760a7b432c7db434c26
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-01 20:30:00 -0300

    Resolução exercício 17

--- Modulo-1/Exercicios/condicionais-backend/17/index.js
+++ Modulo-1/Exercicios/condicionais-backend/17/index.js
@@ -1,8 +1,14 @@
 //valor do produto comprado.
-const valorDoProduto = 100000;
+const valorDoProdutoReais = 1000;
 
 //quantidade de parcelas
 const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+const valorCadaParcela = valorDoProdutoReais / quantidadeDoParcelamento
+const valorQueFaltaPagar = valorDoProdutoReais - valorPago
+const parcelasRestantes = quantidadeDoParcelamento - (valorPago / valorCadaParcela)
+
+console.log(`Restam ${parcelasRestantes} de ${valorCadaParcela}$ Reais`)
