1e6a58f284cee026214749b56e720057efea17f0
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-01 19:28:52 -0300

    Resolução exercício 11 parte 1

--- Modulo-1/Exercicios/condicionais-backend/11/index.js
+++ Modulo-1/Exercicios/condicionais-backend/11/index.js
@@ -1,8 +1,21 @@
 //renda mensal do aluno, em centavos.
-const rendaMensalEmCentavos = 300000;
+const rendaMensalEmCentavos = 3_000_00;
 
 // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
 const mesesDecorridos = 12;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
+
+const rendaMensalEmReais = rendaMensalEmCentavos / 100
+const totalJaPagoPeloAlunoEmReais = totalJaPagoPeloAluno / 100
+const valorParcela = rendaMensalEmReais * (18/100)
+const mesesTotais = 60
+
+if(mesesDecorridos === mesesTotais) {
+  console.log("Mensalidade ISENTA")
+} else if(rendaMensalEmReais > 2000) {
+  console.log(`Mensalidade de ${valorParcela}$`)
+} else {
+  console.log("ERROR")
+}
