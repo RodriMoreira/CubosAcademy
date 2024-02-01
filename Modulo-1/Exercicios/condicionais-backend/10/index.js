58b8749fd534e93ab5d9111e5be294814f7a0521
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-01 19:15:42 -0300

    Resolução exercício 10

--- Modulo-1/Exercicios/condicionais-backend/10/index.js
+++ Modulo-1/Exercicios/condicionais-backend/10/index.js
@@ -1,1 +1,20 @@
-const caractere = "E";
+const caractere = "1";
+
+if(caractere === "a" || 
+   caractere === "e" || 
+   caractere === "i" || 
+   caractere === "o" || 
+   caractere === "u") {
+  console.log("Vogal Minuscula")
+} else if(
+   caractere === "A" || 
+   caractere === "E" || 
+   caractere === "I" || 
+   caractere === "O" || 
+   caractere === "U") {
+  console.log("Vogal Maiúscula")
+} else if(caractere >= 0 || caractere <= 9) {
+  console.log("Número")
+} else {
+  console.log("Consoante")
+}
