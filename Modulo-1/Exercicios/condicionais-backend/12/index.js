9319d46ecd6a8761a870d38df044fe08d586bacb
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-01 19:43:13 -0300

    Resolução exercício 12

--- Modulo-1/Exercicios/condicionais-backend/12/index.js
+++ Modulo-1/Exercicios/condicionais-backend/12/index.js
@@ -1,1 +1,21 @@
 const diaDaSemana = 3;
+
+if(diaDaSemana < 1 || diaDaSemana > 7){
+ console.log("Valor inválido")
+} else if(diaDaSemana === 1) {
+  console.log("Segunda")
+} else if(diaDaSemana === 2) {
+  console.log("Terça")
+} else if(diaDaSemana === 3) {
+  console.log("Quarta")
+} else if(diaDaSemana === 4) {
+  console.log("Quinta")
+} else if(diaDaSemana === 5) {
+  console.log("Sexta")
+} else if(diaDaSemana === 6) {
+  console.log("Sábado")
+} else if(diaDaSemana === 7) {
+  console.log("Domingo")
+} else {
+  console.log("ERROR")
+}
