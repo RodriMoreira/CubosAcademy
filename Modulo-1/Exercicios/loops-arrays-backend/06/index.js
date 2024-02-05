2d9accde7e1221343e4be8ad72a3492b71d7ed96
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-05 17:35:53 -0300

    Resolução exercício 6

--- Modulo-1/Exercicios/loops-arrays-backend/06/index.js
+++ Modulo-1/Exercicios/loops-arrays-backend/06/index.js
@@ -1,2 +1,10 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+let somaNumerosPares = 0;
 
+for(let pares of numeros){
+  if(pares % 2 === 0) {
+    somaNumerosPares += pares
+  }
+}
+
+console.log(somaNumerosPares)
