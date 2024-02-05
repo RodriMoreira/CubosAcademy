fb972197d1711488da6423148df9c05e0b005604
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-05 19:19:34 -0300

    Resolução exercício 12

--- Modulo-1/Exercicios/loops-arrays-backend/12/index.js
+++ Modulo-1/Exercicios/loops-arrays-backend/12/index.js
@@ -1,3 +1,13 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+let passarPessoaParaDentro;
 
+for(let i = 0; i < filaDeFora.length; i++) {
+    passarPessoaParaDentro = filaDeFora[i]
+
+    if(filaDeDentro.length < 5){
+      filaDeDentro.push(passarPessoaParaDentro)
+    }
+  }
+
+console.log(filaDeDentro)
