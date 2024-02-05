078443c79f278dc04f5433cade865c092b9c93ff
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-05 17:17:16 -0300

    Resolução exercício 3

--- Modulo-1/Exercicios/loops-arrays-backend/03/index.js
+++ Modulo-1/Exercicios/loops-arrays-backend/03/index.js
@@ -1,2 +1,15 @@
 const numeros = [54, 22, 14, 87, 10, 284];
+let numeroDez = false
+let indice = -1
 
+for(let numero of numeros) {
+  indice++
+  if(numero === 10){
+    console.log(`O numero está na posição (indice): ${indice}`)
+    numeroDez = true
+  }
+}
+
+if(!numeroDez) {
+  console.log("-1")
+}
