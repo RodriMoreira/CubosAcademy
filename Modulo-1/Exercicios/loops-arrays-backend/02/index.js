10e9d2301cee9936b0bfa6bbe9b2ac048a555a2b
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-05 17:17:01 -0300

    Resolução exercício 2

--- Modulo-1/Exercicios/loops-arrays-backend/02/index.js
+++ Modulo-1/Exercicios/loops-arrays-backend/02/index.js
@@ -1,1 +1,15 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+let letraE = false
+let somaLetraE = 0
+
+for(let letra of letras) {
+  if(letra === "E" || letra === "e") {
+    somaLetraE++
+    letraE = true
+  }
+}
+if(!letraE) {
+  console.log("Nenhuma letra E ou e foi encontrada.")
+}
+
+console.log(`Foram encontradas ${somaLetraE} letras "E" ou "e".`)
