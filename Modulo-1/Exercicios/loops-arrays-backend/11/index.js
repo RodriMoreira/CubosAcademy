4f1dab0996a83d02c471ee06b432649cdc19298b
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-05 18:36:30 -0300

    Resolução exercício 11

--- Modulo-1/Exercicios/loops-arrays-backend/11/index.js
+++ Modulo-1/Exercicios/loops-arrays-backend/11/index.js
@@ -1,2 +1,10 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
+let numeros = []
 
+for(let numero of original) {
+  if((numero >= 10 && numero <= 20) || numero > 100) {
+    numeros.push(numero)
+  }
+}
+
+console.log(numeros)
