6dd331eaf7b8ff25229f12be3466d7d405e12ef6
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-05 17:47:04 -0300

    Resolução exercício 7

--- Modulo-1/Exercicios/loops-arrays-backend/07/index.js
+++ Modulo-1/Exercicios/loops-arrays-backend/07/index.js
@@ -1,1 +1,9 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+let nomesLetraA = [];
+
+for(let nome of nomes) {
+  if(nome[0] === "A" || nome[0] === "a"){
+    nomesLetraA.push(nome)
+  }
+}
+console.log(nomesLetraA)
