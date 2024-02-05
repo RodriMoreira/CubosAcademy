a67b4d2afa85086b0a2e663168b245e8af938ab1
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-05 19:35:01 -0300

    Resolução exercício 13

--- Modulo-1/Exercicios/loops-arrays-backend/13/index.js
+++ Modulo-1/Exercicios/loops-arrays-backend/13/index.js
@@ -1,2 +1,13 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
+let disjuntoresAtivos = []
+let indice = 0
 
+for(let fase of disjuntores) {
+  
+  if(fase){
+    disjuntoresAtivos.push(indice)
+  }
+  indice++
+}
+
+console.log(disjuntoresAtivos)
