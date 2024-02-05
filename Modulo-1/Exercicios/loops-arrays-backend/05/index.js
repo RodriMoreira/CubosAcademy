14a7d759932385a936cf154ae224d7dbab8a21d2
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-05 17:33:42 -0300

    Resolução exercício 5

--- Modulo-1/Exercicios/loops-arrays-backend/05/index.js
+++ Modulo-1/Exercicios/loops-arrays-backend/05/index.js
@@ -1,1 +1,10 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+let arrayCopia = []
+
+for(let pares of original) {
+  if(pares % 2 === 0) {
+    arrayCopia.push(pares)
+  }
+}
+
+console.log(arrayCopia)
