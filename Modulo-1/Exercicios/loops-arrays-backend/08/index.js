eb4a5e15b32fa21e5a997a37fa17f2124b5b231d
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-05 17:57:11 -0300

    Resolução exercício 8

--- Modulo-1/Exercicios/loops-arrays-backend/08/index.js
+++ Modulo-1/Exercicios/loops-arrays-backend/08/index.js
@@ -1,1 +1,11 @@
 const numeros = [3, 24, 1, 8, 11, 7, 15];
+let maiorNumero = 0
+for(let i = 0; i < numeros.length; i++){
+  let numero = numeros[i]
+
+  if(numero > maiorNumero){
+    maiorNumero = numero
+  }
+}
+
+console.log(maiorNumero)
