cca0c684f0570f9f03ade59f9677df5799390774
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-06 20:06:11 -0300

    Ajuste do código

--- Modulo-1/Exercicios/loops-arrays-hackerrank/02/index.js
+++ Modulo-1/Exercicios/loops-arrays-hackerrank/02/index.js
@@ -1,5 +1,5 @@
 const valores = [100,  500, 100, 200, 50]
-let menorNumero = Infinity
+
 let valorTotal = 0
 
 //Soma o valor total
@@ -7,16 +7,22 @@ for(let valor of valores) {
   valorTotal += valor
 }
 
-//Encontra o menor número do array
-for(let valor of valores) {
-  if (valor < menorNumero) {
-    menorNumero = valor
-  }
-}
-
-//Faz a verificação do desconto e imprime o valor final na tela
+//Faz a verificação da quantidade de itens
 if(valores.length >= 5) {
+
+  //Encontra o menor número do array
+  let menorNumero = Infinity
+
+  for(let valor of valores) {
+    if (valor < menorNumero) {
+      menorNumero = valor
+    }
+  }
+
+  //Aplica o desconto ao valor final
   let valorTotalComDesconto =  valorTotal - menorNumero
+
+  //imprimo o valor final com desconto
   console.log(valorTotalComDesconto)
 } else {
   console.log(valorTotal)
