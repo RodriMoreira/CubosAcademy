2420461d552bbdb264716721c0e97fbffe8d831d
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-15 18:59:46 -0300

    Resolução exercício 1

---
+++ Modulo-1/Exercicios/objetos-backend/01/index.js
@@ -0,0 +1,18 @@
+const endereco = {
+  rua: "",
+  numero: "",
+  complemento: "",
+  bairro: "",
+  cep:"",
+  cidade: "",
+  estado: "",
+  pais: ""
+}
+
+const usuario = {
+  nome: "",
+  email: "",
+  telefone: "",
+  dataNascimento: "",
+  endereco: endereco
+}
