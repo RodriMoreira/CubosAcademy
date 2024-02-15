68a9d7188ff305e9f627de852fd41e3349bf69f2
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-15 20:12:32 -0300

    Resolução exercício 5

---
+++ Modulo-1/Exercicios/objetos-backend/05/index.js
@@ -0,0 +1,22 @@
+const curso = {
+  id: 1234,
+  nome: "Lógica de programação",
+  aulas: [
+    aula01 = {
+          identificador: 1,
+          nome_da_aula: "Introdução a programação"
+    },
+    aula02 = {
+          identificador: 2,
+          nome_da_aula: "Variáveis"
+    },
+    aula03 = {
+          identificador: 3,
+          nome_da_aula: "Condicionais"
+    },
+    aula04 = {
+          identificador: 4,
+          nome_da_aula: "Arrays"
+    }
+  ],
+}
