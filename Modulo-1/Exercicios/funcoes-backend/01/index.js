9e527c7ed35bffa6fc857e1a77832dd8369ab0eb
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-20 19:48:09 -0300

    Resolução exercício 1

--- Modulo-1/Exercicios/funcoes-backend/01/index.js
+++ Modulo-1/Exercicios/funcoes-backend/01/index.js
@@ -26,3 +26,20 @@ const prova = {
     ]
 };
 
+let questoesAcertadas = 0
+let nota = 0
+
+
+function corrigirProva() {
+    for(let i = 0; i < prova.questoes.length; i++) {
+       if(prova.questoes[i].resposta === prova.questoes[i].correta) {
+        questoesAcertadas++
+        nota += 2
+       }
+    }
+
+    let resultado = `O aluno(a) ${prova.aluno}, acertou ${questoesAcertadas} e obteve nota ${nota}`
+    console.log(resultado)
+} 
+
+corrigirProva(prova)
