b1cb71e41f8596116ff38ab45d65404b6086c218
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-26 18:42:32 -0300

    Métodos de strings 2

--- Modulo-1/Aulas/metodosStrings.js
+++ Modulo-1/Aulas/metodosStrings.js
@@ -7,11 +7,16 @@ texto.length;
 //string.includes(texto, [posição])
 texto.includes("Cubos");
 
-//Encontrar o PRIMEIRO texto dentro de uma string, gera um valor referente ao índice, -1 == false
+//Encontrar o PRIMEIRO texto dentro de uma string, gera um valor referente ao índice, -1 == Não encontrado
 //string.indexOf(texto, [posição])
 texto.indexOf("Cubos");
 
-//Encontrar o ULTIMO texto dentro de uma string, gera um valor referente ao índice, -1 == false
+//Encontrar o ULTIMO texto dentro de uma string, gera um valor referente ao índice, -1 == Não encontrado
 //LastIndex faz a busca de trás para frente, tomar CUIDADO com o parâmetro de posição
 //string.indexOf(texto, [posição])
 texto.lastIndexOf("Cubos");
+
+//Recortar e armazenar um pedaço de uma string
+//string.slice(posição 1, [posição 2])
+//Números negativos fazem a busca de trás para frente
+texto.slice(0)
