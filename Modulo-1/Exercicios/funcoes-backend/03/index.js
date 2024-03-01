3848b9fed6dfbab795404ed63931c6d6bf2ab395
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-02-29 21:10:21 -0300

    Exercício 3 Imcompleto

--- Modulo-1/Exercicios/funcoes-backend/03/index.js
+++ Modulo-1/Exercicios/funcoes-backend/03/index.js
@@ -5,13 +5,70 @@ const carrinho = {
             id: 1,
             nome: "Camisa",
             qtd: 3,
-            precoUnit: 3000
+            precoUnit: 30
         },
         {
             id: 2,
             nome: "Bermuda",
             qtd: 2,
-            precoUnit: 5000
+            precoUnit: 50
         }
-    ]
+    ],
+
+    imprimirResumo: function() {    
+        console.log(`Cliente: ${this.nomeDoCliente} \n Total de itens: ${this.calcularTotalItens()} \n Total a pagar: ${this.CalcularPrecoFinal()} `)
+    },
+
+    imprimirResumoDetalhado: function() {
+    
+        function ItensDetalhados() {
+            let texto = ``
+            for(let i = 0; i < carrinho.produtos.length; i++) {
+                texto += `Item: ${i + 1} - ${carrinho.produtos[i].nome} - ${carrinho.produtos[i].qtd} - R$ ${carrinho.produtos[i].precoUnit.toFixed(2)}\n`
+            }
+           return texto
+        }
+        let textoDetalhado = `Cliente: ${this.nomeDoCliente}\n\n${ItensDetalhados()}\nTotal de itens: ${this.calcularTotalItens()}\nTotal a pagar: ${this.CalcularPrecoFinal()}`
+        console.log(textoDetalhado)
+    },
+
+    addCarrinho: function(produto) {
+        this.produtos.push(produto)
+    },
+
+    calcularTotalItens: function() {
+        let quantidadeItens = 0
+        for(let i = 0; i < this.produtos.length; i++) {
+            quantidadeItens += this.produtos[i].qtd
+        }
+        return quantidadeItens
+    },
+
+    CalcularPrecoFinal: function() {
+        let precoFinal = 0
+        for(let i = 0; i < this.produtos.length; i++) {
+            precoFinal += this.produtos[i].precoUnit * this.produtos[i].qtd
+        }
+        return precoFinal.toFixed(2)
+    },
+
+
 }
+
+const novoTenis = {
+    id: 3,
+    nome: "Tenis",
+    qtd: 1,
+    precoUnit: 100
+}
+
+const novaBermuda = {
+    id: 2,
+    nome: "Bermuda",
+    qtd: 3,
+    precoUnit: 50
+}
+
+carrinho.addCarrinho(novoTenis)
+carrinho.addCarrinho(novaBermuda)
+carrinho.imprimirResumoDetalhado()
