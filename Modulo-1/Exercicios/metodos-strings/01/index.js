dd2481702af85a6d9c466f848dc8ea0e4eaaec19
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-04 17:18:04 -0300

    Resolução exercício 1

--- Modulo-1/Exercicios/metodos-strings/01/index.js
+++ Modulo-1/Exercicios/metodos-strings/01/index.js
@@ -1,1 +1,13 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "Esse CoViD é muito perigoso!";
+ProibirPalavra(comentario)
+
+
+function ProibirPalavra(string) {
+  const stringFormatada = string.toLowerCase()
+
+  if(stringFormatada.includes("covid") || stringFormatada.includes("pandemia")){
+    console.log("Comentário bloqueado por conter palavras proibidas");
+  } else {
+    console.log("Comentário autorizado");
+  }
+}
