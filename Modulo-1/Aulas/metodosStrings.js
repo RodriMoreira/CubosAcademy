9f4a0c1c3a5cca222a6e584574e6e4bbca66bce7
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-04 17:18:49 -0300

    Ajuste de conteúdo

--- Modulo-1/Aulas/metodosStrings.js
+++ Modulo-1/Aulas/metodosStrings.js
@@ -1,4 +1,8 @@
+//METODOS DE STRINGS QUE ESPERAM TEXTOS NÃO PODEM RECEBER NUMEROS COMO ARUMENTO
 const texto = "Cubos academy"
+
+//transforma o argumento em string
+String(texto)
 
 //Tamanho da string
 texto.length;
@@ -22,4 +26,39 @@ texto.lastIndexOf("Cubos");
 texto.slice(0)
 
 //Transformar texto em array
-texto.split("")
+//EX: Jósé Messias Pereira ==> ["José", "Messias", "Pereira"]
+const array = texto.split("")
+
+//Substitui a palavra da posição 1 pela palavra da posição 2
+texto.replace("Cubos", "cubos")
+//Função que consegue substituir todas as palavras "antigas" por palavras "Novas", já que o replace só funciona com a primeira palavra encontrada, a função repete o replace até substituir todas
+function replaceAll(textoOriginal, textoAntigo, textoNovo) { 
+  while (textoOriginal !== textoOriginal.replace(textoAntigo, textoNovo)) {
+    textoOriginal = textoOriginal.replace(textoAntigo, textoNovo)
+  }  
+}
+
+//Transforma as letras de uma string em letras maiusculas
+texto.toUpperCase()
+
+//Transforma as letras de uma string em letras minusculas
+texto.toLowerCase()
+
+//remove espaços no começo e no fim da string
+texto.trim()
+
+//PadStart preenche a string com um texto escolhido
+//.padStart(tamanho que a string deve ter, "Texto que vai entrar na string até atingir o tamanho")
+texto.padStart(1, "")
+
+const ultimosQuatroDigitosCartao = "2345";
+const numeroCartaoOculto = ultimosQuatroDigitosCartao.padStart(19, "**** ")
+
+function imprimirData(dia, mes, ano)  {
+  const diaFormulado = String(dia).padStart(2, "0")
+  const mesFormulado = String(mes).padStart(2, "0")
+
+  console.log(`${diaFormulado}/${mesFormulado}/${ano}`);
+}
+
+imprimirData(1,1,2001)
