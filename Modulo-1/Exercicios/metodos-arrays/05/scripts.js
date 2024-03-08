9e002f263d23f15030f20fce49308abaad0f5820
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-08 20:44:58 -0300

    Resolução exercício 5

--- Modulo-1/Exercicios/metodos-arrays/05/scripts.js
+++ Modulo-1/Exercicios/metodos-arrays/05/scripts.js
@@ -1,1 +1,20 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+function atenderPaciente(pacientes) {
+  pacientes.shift()
+  console.log(pacientes);
+}
+
+function agendarPaciente(pacientes) {
+  pacientes.push()
+  console.log(pacientes);
+}
+
+function cancelarPaciente(paciente, nome) {
+  const indice = paciente.indexOf(nome)
+  paciente.splice(indice, 1)
+
+  console.log(paciente);
+}
+
+cancelarPaciente(pacientes, "Pedro")
