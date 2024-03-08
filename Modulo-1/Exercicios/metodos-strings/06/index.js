7c96ec551f307d395fea94b594bb646aac077fb7
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-08 17:27:51 -0300

    Resolução exercício 6

--- Modulo-1/Exercicios/metodos-strings/06/index.js
+++ Modulo-1/Exercicios/metodos-strings/06/index.js
@@ -1,1 +1,16 @@
-const celular = 7199918888;
+const celular = "2199918888";
+
+function formatarNumeroCelular(celular) {
+  let numeroFormatado = ""
+
+  if(celular.length == 8) {
+    numeroFormatado += `9 ${celular.slice(0, 4)}-${celular.slice(4, 8)}`
+
+  } else if(celular.length == 10) {
+    numeroFormatado += `(${celular.slice(0, 2)}) 9 ${celular.slice(2, 6)}-${celular.slice(6, 10)}`
+  }
+
+  return console.log(numeroFormatado);
+}
+
+formatarNumeroCelular(celular)
