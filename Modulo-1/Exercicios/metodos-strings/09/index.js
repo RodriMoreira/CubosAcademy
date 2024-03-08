8f5c81632b258fd01c254a2854c9de8cee3c383c
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-08 18:15:03 -0300

    Resolução exercício 9

--- Modulo-1/Exercicios/metodos-strings/09/index.js
+++ Modulo-1/Exercicios/metodos-strings/09/index.js
@@ -1,1 +1,22 @@
 const nome = 'Guido Cerqueira';
+
+function criarNick(nome) {
+  let array = []
+  let nickname = ""
+
+  while(nome !== nome.replace(" ", "")) {
+    nome = nome.replace(" ", "")
+  }
+
+  nome.toLowerCase()
+  const nomeCorrigido = nome.slice(0, 12)
+
+  array = nomeCorrigido.split(" ")
+  array.unshift("@")
+
+  nickname = array.join("")
+  
+  return console.log(nickname);
+}
+
+criarNick(nome)
