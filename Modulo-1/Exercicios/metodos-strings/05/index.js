f8333e1cf7faa28bf40258db0cd63ffd76b8d96a
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-08 17:10:58 -0300

    Resolução exercício 5

--- Modulo-1/Exercicios/metodos-strings/05/index.js
+++ Modulo-1/Exercicios/metodos-strings/05/index.js
@@ -1,1 +1,12 @@
-const numeroCartao = '1111222233334444';
+const numeroCartao = '1111222233335555';
+
+function esconderNumero(numeroCartao) {
+  let numeroAlterado = ""
+  const primeirosNumeros = numeroCartao.slice(0, 4);
+  const ultimosNumeros = numeroCartao.slice(-4);
+  
+  numeroAlterado += primeirosNumeros + numeroAlterado.padEnd(8, "*") + ultimosNumeros
+  return console.log(numeroAlterado);
+}
+
+esconderNumero(numeroCartao)
