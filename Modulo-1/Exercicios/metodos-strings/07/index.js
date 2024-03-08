2dc0d8503b31d2afbd5b61cab0b38d6d972d4544
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-08 17:43:49 -0300

    Resolução exercício 7

--- Modulo-1/Exercicios/metodos-strings/07/index.js
+++ Modulo-1/Exercicios/metodos-strings/07/index.js
@@ -1,1 +1,22 @@
 const email = "aluno@cubos.academy";
+
+function formatarEmail(email) {
+  let confirmacao = ""
+  const verificacao = email.includes("@") && 
+                      email.includes(".") &&
+                      !(email.includes(" ")) &&
+                      !(email.charAt(0) == ".") &&
+                      !(email.charAt(email.length - 1) == ".")
+
+  if(verificacao) {
+    confirmacao = "Email Válido"
+  } else {
+    confirmacao = "Email inválido"
+  }
+
+  return console.log(confirmacao);
+}
+
+formatarEmail(email)
+
+
