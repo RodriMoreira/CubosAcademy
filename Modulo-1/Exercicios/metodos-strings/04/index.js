cd9952d14856738623704b1a8342f6878db6d281
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-08 16:44:24 -0300

    Resolução exercício 4

--- Modulo-1/Exercicios/metodos-strings/04/index.js
+++ Modulo-1/Exercicios/metodos-strings/04/index.js
@@ -1,3 +1,29 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+function identificadorCorrigido(identificador) {
+  let identificadorCorrigido = identificador.padStart(6, "0")
+  return console.log(identificadorCorrigido);
+}
+
+function nomeCapitalizado(nome) {
+  let palavras = nome.split(" ")
+  let palavrasCapitalizadas = []
+
+  for(palavra of palavras) {
+    palavrasCapitalizadas.push(palavra.charAt(0).toUpperCase() + palavra.slice(1))
+  }
+
+  return console.log(palavrasCapitalizadas.join(" "));
+}
+
+
+function emailCorrigido(email) {
+  return console.log(email.trim());
+}
+identificadorCorrigido(identificador)
+nomeCapitalizado(nome)
+emailCorrigido(email)
+
+
