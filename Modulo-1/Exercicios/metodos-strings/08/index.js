166f14c022227526ddaab61d092008741316f941
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-08 17:54:22 -0300

    Resolução exercício 8

--- Modulo-1/Exercicios/metodos-strings/08/index.js
+++ Modulo-1/Exercicios/metodos-strings/08/index.js
@@ -1,1 +1,19 @@
-const cpf = "011.022.033-44";
+const cpf = "12.345.678/9001-99";
+
+function formatar(texto) {
+  while(texto !== texto.replace(".", "")) {
+    texto = texto.replace(".", "")
+  }
+
+  while(texto !== texto.replace("-", "")) {
+    texto = texto.replace("-", "")
+  }
+
+  while(texto !== texto.replace("/", "")) {
+    texto = texto.replace("/", "")
+  }
+
+  return console.log(texto);
+}
+
+formatar(cpf)
