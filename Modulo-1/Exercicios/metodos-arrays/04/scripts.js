fde9265821b63dc142706c76eec0eb01150a20a2
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-08 20:38:39 -0300

    Resolução exercício 4

--- Modulo-1/Exercicios/metodos-arrays/04/scripts.js
+++ Modulo-1/Exercicios/metodos-arrays/04/scripts.js
@@ -1,1 +1,18 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+function listaDePacientes(pacientes,operacao,nome) {
+
+  if(operacao == "agendar") {
+    pacientes.push(nome)
+  } 
+
+  if(operacao == "atender") {
+    pacientes.shift()
+  } 
+
+  console.log(pacientes.join(", "));
+}
+
+listaDePacientes(pacientes, "atender")
+listaDePacientes(pacientes, "agendar", "rodrigo")
+listaDePacientes(pacientes, "atender")
