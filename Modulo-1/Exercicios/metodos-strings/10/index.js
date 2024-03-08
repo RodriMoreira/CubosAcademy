474f6f919b218c6f70142f8a59dfc515c4cb9b0a
Rodrigo Moreira <rodmoreira1312@gmail.com>
2024-03-08 18:24:17 -0300

    Resolução exercício 10

--- Modulo-1/Exercicios/metodos-strings/10/index.js
+++ Modulo-1/Exercicios/metodos-strings/10/index.js
@@ -1,1 +1,19 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.png';
+
+function Arquivo(arquivo) {
+  let tipoArquivo = ""
+  const verificacao = arquivo.includes(".jpg") ||
+                      arquivo.includes(".jpeg") ||
+                      arquivo.includes(".gif") ||
+                      arquivo.includes(".png") 
+
+  if(verificacao) {
+    tipoArquivo = "arquivo válido"
+  } else {
+    tipoArquivo = "arquivo inválido" 
+  }
+
+  return console.log(tipoArquivo);
+}
+
+Arquivo(nomeArquivo)
